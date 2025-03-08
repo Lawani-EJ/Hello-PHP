e0564364845cee79f05a9635df941c94857f4cc6
Lawani-EJ <elyonjohn60@gmail.com>
2025-03-08 12:50:40 +0100

    Creating comments and adding variables

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables in PHP</title>
+</head>
+<body>    
+<?php 
+// A comment can be written like this
+# And a comment can be written like this too
+/**And a multiple line comment can be written like this too */
 echo "HELLO WORLD";
+
+# Variable 
+# A variable in PHP is used to store data , or its the name of the memoruy location that holds the data
+$language = "PHP";
+
+echo $language
 ?>
+</body>
+</html>
